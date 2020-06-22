8b8788f07574e4c2af7db391e56a8e0c8e1b7ec1
Aluis Marte <aluismarte@gmail.com>
2020-06-21 22:50:02 -0400

    Agregando licensia y actualizando.

--- src/main/java/edu/alsjava/example/vaadinsecurity/util/security/SecurityConfiguration.java
+++ src/main/java/edu/alsjava/example/vaadinsecurity/util/security/SecurityConfiguration.java
@@ -11,7 +11,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
  * Created by aluis on 5/10/20.
